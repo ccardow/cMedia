8c85470694e80eaa72149eba282d5ef37177a235
ccardow <ccardow@gmail.com>
2018-06-19 20:46:12 -0700

    Added BrowserSync

--- gulpfile.js
+++ gulpfile.js
@@ -1,35 +1,46 @@
-var gulp = require('gulp'), 
-watch = require ('gulp-watch'),
+var gulp = require('gulp'),
+watch = require('gulp-watch'),
 postcss = require('gulp-postcss'),
-autoprefixer = require ('autoprefixer'),
-cssvars = require ('postcss-simple-vars'),
-nested = require ('postcss-nested'),
-cssImport = require ('postcss-import');
+autoprefixer = require('autoprefixer'),
+cssvars = require('postcss-simple-vars'),
+nested = require('postcss-nested'),
+cssImport = require('postcss-import'),
+browserSync = require('browser-sync').create();
 
-gulp.task('default', function(){
-    console.log('hooray');
+gulp.task('default', function() {
+  console.log("Hooray - you created a Gulp task.");
 });
 
-gulp.task('html', function(){
-    console.log ('html changes here');
+gulp.task('html', function() {
+  console.log("Imagine something useful being done to your HTML here.");
 });
 
-gulp.task('styles', function(){
-    //where to get 
-    return gulp.src('./app/assets/styles/styles.css')
-    //the pipe
+gulp.task('styles', function() {
+  return gulp.src('./app/assets/styles/styles.css')
     .pipe(postcss([cssImport, cssvars, nested, autoprefixer]))
-    //the destination
-    .pipe(gulp.dest('./app/temp/styles/'));
+    .pipe(gulp.dest('./app/temp/styles'));
 });
 
-gulp.task('watch', function(){
-    
-    watch('./app/index.html', function(){
-        gulp.start('html');
-    });
+gulp.task('watch', function() {
 
-    watch('./app/assets/styles/**/*.css', function() {
-        gulp.start('styles');
-    });
+  browserSync.init({
+    notify: false,
+    server: {
+      baseDir: "app"
+    }
+  });
+
+  watch('./app/index.html', function() {
+    browserSync.reload();
+  });
+
+  watch('./app/assets/styles/**/*.css', function() {
+    gulp.start('cssInject');
+  });
+
+});
+
+gulp.task('cssInject', ['styles'], function() {
+  return gulp.src('./app/temp/styles/styles.css')
+    .pipe(browserSync.stream());
 });
