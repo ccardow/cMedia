0a3e88012e009f4388e93278bd93569173ef42f6
Cameron Cardow <ccardow@gmail.com>
2019-07-18 18:59:59 -0700

    modal completed.

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -3,9 +3,11 @@ import RevealOnScroll from "./modules/RevealOnScroll";
 import AnimateOnScroll from "./modules/AnimateOnScroll";
 import $ from "jquery";
 import StickyHeader from "./modules/StickyHeader";
+import Modal from "./modules/Modal";
 
 const mobileMenu = new MobileMenu();
 new RevealOnScroll($(".feature-item"), "85%");
 new AnimateOnScroll($(".skillBar"), "85%");
 new RevealOnScroll($(".testimonials"), "60%");
 const stickyHeader = new StickyHeader();
+const modal = new Modal();
